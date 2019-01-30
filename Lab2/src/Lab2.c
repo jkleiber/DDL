e9edf85f876851211b8bdb00c7c11e8c04b2fba6
Justin Kleiber <jkleiber8@gmail.com>
2019-01-29 18:26:13 -0600

    Adds GPIO defs, GPIO reading, updates ports list to be an array, and outlines skeleton code for future development
    Comments the code so it is understandable in the future

--- Lab2/src/Lab2.c
+++ Lab2/src/Lab2.c
@@ -1,101 +1,122 @@
-#define FIO0DIR (*(volatile unsigned int *)0x2009c000)
-#define FIO0PIN (*(volatile unsigned int *)0x2009c014)
-
-int turn = 0;
-
-void lights(int row, int num)
+//Declare the GPIO_regs struct
+typedef struct GPIO_regs_t
 {
-	switch(row)
-	{
-	//Red LEDs port p01,p00,p06,p07,p08,p09,p018
-	case 0:
-		switch(num)
-		{
-		case 1:
+    volatile unsigned int FIODIR;
+    int rsvd[3];    // TODO: check the user manual to see if this is supposed to be 3 pad bytes or just 1
+    volatile unsigned int FIOMASK;
+    volatile unsigned int FIOPIN;
+    volatile unsigned int FIOSET;
+    volatile unsigned int FIOCLR;
+} GPIO_regs;
 
-			break;
-		case 2:
+//Define FIO
+#define FIO ((GPIO_regs *)0x2009c000)
 
-			break;
-		case 3:
+/* Set the port/pin combos used to control the LEDs*/
+//Red LEDs located at p00, p01, p06, p07, p08, p09, p018
+const int red_ports[] = {0, 0, 0, 0, 0, 0,  0};
+const int red_pins[]  = {0, 1, 6, 7, 8, 9, 18};
 
-			break;
-		case 4:
+//Green LEDs located at p017, p015, p016, p023, p024
+const int green_ports[] = { 0,  0,  0,  0,  0};
+const int green_pins[]  = {17, 15, 16, 23, 24};
 
-			break;
-		case 5:
+//Blue LEDs located at p025, p026, p02
+const int blue_ports[] = { 0,  0, 0};
+const int blue_pins[]  = {25, 26, 2};
 
-			break;
-		case 6:
+/* Set the port/pin combos for the buttons */
+// TODO: choose pins and implement
 
-			break;
-		case 7:
+/**
+ * bus_out_write - Function that can write a series of bits to a set of pins.
+ * The maximum number of bits this function can write is the size of unsigned int
+ * 
+ * @param bits: bits to set
+ * @param *ports: list of ports to set (corresponds to pins)
+ * @param *pins: list of pins to alter based on bit list
+ * @param num_bits: number of bits to write
+ */
+void bus_out_write(unsigned int bits, int *ports, int *pins, int num_bits)
+{
 
-			break;
-		default:
-
-			break;
-		}
-		break;
-		//Blue LEDs port p017,p015,p016,p023,p024
-	case 1:
-		switch(num)
-		{
-		case 1:
-
-		    break;
-		case 2:
-
-		    break;
-		case 3:
-
-		    break;
-		case 4:
-
-		    break;
-		case 5:
-
-		    break;
-		default:
-
-		    break;
-		}
-		break;
-		//Blue LEDs port p025,p026,p02
-	case 2:
-		switch(num)
-		{
-		case 1:
-
-		    break;
-		case 2:
-
-		    break;
-		case 3:
-
-		    break;
-		default:
-
-		    break;
-		}
-		break;
-
-	default:
-		break;
-
-	}
 }
 
-int main(void) {
+/**
+ * read_single - Function that reads a single port and pin combination
+ *               and returns the current state
+ */
+unsigned int read_single(int port, int pin)
+{
+    return ((FIO[port].FIOPIN >> pin) & 1);
+}
 
-	int red = 0;
-	int green = 0;
-	int blue = 0;
+/**
+ * bus_in_read - Function that can read input from a list of pins.
+ * 
+ * @param *ports: list of ports to read (corresponds to pins)
+ * @param *pins: list of pins to read from
+ * @param num_pins: number of pins to read
+ * @return: an unsigned int with the readings from the bus
+ */
+unsigned int bus_in_read(int *ports, int *pins, int num_pins)
+{
+    //Declare and initialize local variables
+    int output = 0; 
 
-    while(1){
+    //Iterate through each port/pin combo and read data
+    for(int i = 0; i < num_pins; ++i)
+    {
+        //If the pin value is HIGH, set this bit to 1
+        if(read_single(ports[i], pins[i]))
+        {
+            output = (output << 1) | 1;
+        }
+        //Otherwise set this bit to 0
+        else
+        {
+            output = output << 1;
+        }
+    }
 
+    return output;
+}
 
+/**
+ * Main function
+ */
+int main(void) 
+{
+    /* Declare and initialize local variables*/
+    int turn = 0;       //tracks the players turns (0 or 1)
+    int curButton = -1; //tracks the last button press
+    int lastTurn = -1;  //tracks who moved last
+	int red = 0;        //tracks how many red LEDs are off using bit shifting
+	int green = 0;      //tracks how many green LEDs are off using bit shifting
+	int blue = 0;       //tracks how many blue LEDs are off using bit shifting
 
+    /* Run the program forever */
+    while(1)
+    {
+        //If it is player 0's turn
+        if(turn == 0)
+        {
+            //Check player 0's buttons
+            //If it is the first time this player has pressed a button (lastTurn != 0) allow any button press
+            //Otherwise only allow the current button
+                //If the other player presses a button then it is now their turn
+                turn = 1;
+        }
+        //Otherwise it is player 1's turn
+        else
+        {
+            //Check player 1's buttons
+            //If it is the first time this player has pressed a button (lastTurn != 1) allow any button press
+            //Otherwise only allow the current button
+                //If the other player presses a button then it is now their turn
+                turn = 0;
+        }
+        
     }
 
 }
