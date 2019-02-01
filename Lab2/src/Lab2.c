05a66ed74bc3802e6b9854d138eac6bfd9cb5e0a
Justin Kleiber <jkleiber8@gmail.com>
2019-01-31 22:09:45 -0600

    Fixes minor syntax errors

--- Lab2/src/Lab2.c
+++ Lab2/src/Lab2.c
@@ -2,7 +2,7 @@
 typedef struct GPIO_regs_t
 {
     volatile unsigned int FIODIR;
-    int                   rsvd[3];
+    int rsvd[3];
     volatile unsigned int FIOMASK;
     volatile unsigned int FIOPIN;
     volatile unsigned int FIOSET;
@@ -14,32 +14,32 @@ typedef struct GPIO_regs_t
 #define FIO ((GPIO_regs *)0x2009c000)
 
 //Define the PINMODE registers
-#define PINMODE0 (*(volatile int *) 0x4002C040)
-#define PINMODE1 (*(volatile int *) 0x4002C044)
-#define PINMODE2 (*(volatile int *) 0x4002C048)
-#define PINMODE3 (*(volatile int *) 0x4002C04C)
-#define PINMODE4 (*(volatile int *) 0x4002C050)
+#define PINMODE0 (*(volatile int *)0x4002C040)
+#define PINMODE1 (*(volatile int *)0x4002C044)
+#define PINMODE2 (*(volatile int *)0x4002C048)
+#define PINMODE3 (*(volatile int *)0x4002C04C)
+#define PINMODE4 (*(volatile int *)0x4002C050)
 
 /* Set the port/pin combos used to control the LEDs*/
 //Red LEDs located at p00, p01, p06, p07, p08, p09, p018
-const int red_ports[] = {0, 0, 0, 0, 0, 0,  0};
-const int red_pins[]  = {0, 1, 6, 7, 8, 9, 18};
+const int red_ports[] = {0, 0, 0, 0, 0, 0, 0};
+const int red_pins[] = {0, 1, 6, 7, 8, 9, 18};
 
 //Green LEDs located at p017, p015, p016, p023, p024
-const int green_ports[] = { 0,  0,  0,  0,  0};
-const int green_pins[]  = {17, 15, 16, 23, 24};
+const int green_ports[] = {0, 0, 0, 0, 0};
+const int green_pins[] = {17, 15, 16, 23, 24};
 
 //Blue LEDs located at p025, p026, p02
-const int blue_ports[] = { 0,  0, 0};
-const int blue_pins[]  = {25, 26, 2};
+const int blue_ports[] = {0, 0, 0};
+const int blue_pins[] = {25, 26, 2};
 
 //Left Button Ports located at p210, p211, p212
-const int left_button_ports[] = { 2,  2, 2};
-const int left_button_pins[]  = {10, 11, 12};
+const int left_button_ports[] = {2, 2, 2};
+const int left_button_pins[] = {10, 11, 12};
 
 //Right Button Ports located at p04, p05, p010
-const int right_button_ports[] = { 0,  0, 0};
-const int right_button_pins[]  = {4, 5, 10};
+const int right_button_ports[] = {0, 0, 0};
+const int right_button_pins[] = {4, 5, 10};
 
 /**
  * bus_out_write - Function that can write a series of bits to a set of pins.
@@ -52,15 +52,17 @@ const int right_button_pins[]  = {4, 5, 10};
  */
 void bus_out_write(unsigned int bits, int *ports, int *pins, int num_bits)
 {
-    for(i=0; i<num_bits; i++)
-     if((bits>>i)&i)
+    for (int i = 0; i < num_bits; i++)
+    {
+        if ((bits >> i) & i)
         {
-            FIO[ports[i]].FIOPIN |= (1<<pins[i]);
+            FIO[ports[i]].FIOPIN |= (1 << pins[i]);
         }
-     else
+        else
         {
-            FIO[ports[i]].FIOPIN &= ~(1<<pins[i]);
+            FIO[ports[i]].FIOPIN &= ~(1 << pins[i]);
         }
+    }
 }
 
 /**
@@ -83,13 +85,13 @@ unsigned int read_single(int port, int pin)
 unsigned int bus_in_read(int *ports, int *pins, int num_pins)
 {
     //Declare and initialize local variables
-    int output = 0; 
+    int output = 0;
 
     //Iterate through each port/pin combo and read data
-    for(int i = 0; i < num_pins; ++i)
+    for (int i = 0; i < num_pins; ++i)
     {
         //If the pin value is HIGH, set this bit to 1
-        if(read_single(ports[i], pins[i]))
+        if (read_single(ports[i], pins[i]))
         {
             output = (output << 1) | 1;
         }
@@ -106,74 +108,91 @@ unsigned int bus_in_read(int *ports, int *pins, int num_pins)
 /**
  * Main function
  */
-int main(void) 
+int main(void)
 {
     /* Declare and initialize local variables*/
     int turn = 0;       //tracks the players turns (0 or 1)
     int curButton = -1; //tracks the last button press
     int lastTurn = -1;  //tracks who moved last
-	int red = 0;        //tracks how many red LEDs are off using bit shifting
-	int green = 0;      //tracks how many green LEDs are off using bit shifting
-	int blue = 0;       //tracks how many blue LEDs are off using bit shifting
+    int red = 0;        //tracks how many red LEDs are off using bit shifting
+    int green = 0;      //tracks how many green LEDs are off using bit shifting
+    int blue = 0;       //tracks how many blue LEDs are off using bit shifting
     int latch = -1;     //triggers to track which butten the player can press on their turn
     int othButton = -1; //
 
     /* Run the program forever */
-    while(1)
+    while (1)
     {
         //If it is player 0's turn
-        if(turn == 0)
+        if (turn == 0)
         {
+            //
             othButton = 0;
-            do 
+
+            //
+            do
             {
-                curButton = bus_in_read(left_button_ports[i], left_button_pins[i]);
-                if(curButton == latch || (curButton == 1 && latch == -1))
+                //
+                curButton = bus_in_read(left_button_ports, left_button_pins, 3);
+
+                //TODO: make this a function
+                if (curButton == latch || (curButton == 1 && latch == -1))
                 {
                     latch = 1;
                     blue <<= 1;
                 }
-                else if(curButton == latch || (curButton == 2 && latch == -1))
+                else if (curButton == latch || (curButton == 2 && latch == -1))
                 {
                     latch = 2;
                     green <<= 1;
                 }
-                else if(curButton == latch || (curButton == 4 && latch == -1))
+                else if (curButton == latch || (curButton == 4 && latch == -1))
                 {
                     latch = 4;
                     red <<= 1;
                 }
-                othButton = bus_in_read(right_button_ports[i], right_button_pins[i],3);
-            }
-            while(othButton==0)
+
+                //
+                othButton = bus_in_read(right_button_ports, right_button_pins, 3);
+
+            } while (othButton == 0);
+
             turn = 1;
         }
         else
         {
+            //
             othButton = 0;
-            do 
+
+            //
+            do
             {
-                curButton = bus_in_read(right_button_ports[i], right_button_pins[i]);
-                if(curButton == latch || (curButton == 1 && latch == -1))
+                //
+                curButton = bus_in_read(right_button_ports, right_button_pins, 3);
+
+                //TODO: make this a function
+                if (curButton == latch || (curButton == 1 && latch == -1))
                 {
                     latch = 1;
                     blue <<= 1;
                 }
-                else if(curButton == latch || (curButton == 2 && latch == -1))
+                else if (curButton == latch || (curButton == 2 && latch == -1))
                 {
                     latch = 2;
                     green <<= 1;
                 }
-                else if(curButton == latch || (curButton == 4 && latch == -1))
+                else if (curButton == latch || (curButton == 4 && latch == -1))
                 {
                     latch = 4;
                     red <<= 1;
                 }
-                othButton = bus_in_read(left_button_ports[i], left_button_pins[i],3);
-            }
-            while(othButton==0)
+
+                //
+                othButton = bus_in_read(left_button_ports, left_button_pins, 3);
+
+            } while (othButton == 0);
+
             turn = 1;
         }
     }
-
 }
