1b428580ebcdf1889248d839111cecef3ea24ce1
aschuman6 <andrea.l.schuman-1@ou.edu>
2019-03-02 14:35:38 -0600

    added function to output to MCP23017 that does not work. added interrupt enable

--- Lab4/src/Lab4.c
+++ Lab4/src/Lab4.c
@@ -3,17 +3,29 @@
 #define I2C_SCLH (*(volatile int *)0x4001C010)
 #define I2C_CONCLR (*(volatile int *)0x4001C018)
 #define I2C_CONSET (*(volatile int *)0x4001C000)
-#define I2STAT (*(volatile int *)0x4001C004)
 #define I2C_DAT (*(volatile int *)0x4001C008)
-#define I2C_FEED() I2C_CONCLR = (1<<3); while(I2C_CONSET>>3 & 1){}
+#define I2C_STAT (*(volatile int *)0x4001C004)
+#define I2C_FEED() I2C_CONCLR = (1<<3); while(((I2C_CONSET>>3) & 1) == 0){}
+#define I2STAT (*(volatile int *)0x4001C004)
+#define ISER (*(volatile int *) 0xE000E100)
 #define LM75ADDR 0x48
 #define MCPADDR 0x20
 #define READ 1
 #define WRITE 0
 
+int stat;
+
+void wait_ticks(int count)
+{
+    volatile int ticks = count;
+    while(ticks>0)
+        ticks--;
+}
+
 void I2C_init(void)
 {
     I2C_CONSET = 0x40;
+
 }
 
 void I2C_start(void)
@@ -53,6 +65,40 @@ void I2C_stop(void)
     {}
 }
 
+void I2C_interrupt(void)
+{
+//    ISER |= 1<<10;
+}
+
+void I2C_bank(void)
+{
+    I2C_start();
+    I2C_write(MCPADDR<<1 & 0);
+    I2C_write(0x0A);
+    I2C_start();
+    I2C_write(0x80);
+    I2C_stop();
+}
+
+void write_Num(int num)
+{
+    stat = I2C_STAT;
+    //Write to MCPADDR
+    I2C_start();
+    stat = I2C_STAT;
+    I2C_write(MCPADDR<<1 & 0);
+    stat = I2C_STAT;
+    I2C_write(0x12);
+    stat = I2C_STAT;
+    I2C_start();
+    stat = I2C_STAT;
+    if(num == 0)
+    {
+        I2C_write(0b1111);
+    }
+    I2C_stop();
+}
+
 int main(void) 
 {
     //Set SCL and SDA0 on PINSEL1
@@ -62,13 +108,14 @@ int main(void)
     I2C_SCLL = 5;
     I2C_SCLH = 5;
 
+    I2C_interrupt();
     //Initialize I2C
     I2C_init();
-
+    I2C_bank();
 
     while(1)
     {
-
+        write_Num(0);
     }
 
 }
