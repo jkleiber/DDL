5349d874af159dd2c5eeba35404024864ed73093
jkleiber <jkleiber8@gmail.com>
2019-03-28 15:54:46 -0500

    Adds speaker functionality and completes lab 5

--- Lab5/src/Lab5.c
+++ Lab5/src/Lab5.c
@@ -5,6 +5,9 @@
 #define AD0INTEN (*(volatile int *) 0x4003400C)
 #define AD0STAT (*(volatile int *) 0x40034030)
 
+//Define DAC registers
+#define DACR (*(volatile int *) 0x4008C000)
+
 //Clock registers
 #define PCLKSEL0 (*(volatile int *) 0x400FC1A8)
 
@@ -15,10 +18,35 @@
 //GPIO pins
 #define FIO0DIR (*(volatile unsigned int *) 0x2009C000)
 #define FIO0PIN (*(volatile unsigned int *) 0x2009C014)
+#define FIO2DIR (*(volatile unsigned int *) 0x2009C040)
+#define FIO2PIN (*(volatile unsigned int *) 0x2009C054)
 
 //Threshold for Light sensor
 #define LIGHT_THRESH 1600
 
+/**
+ * @brief 
+ * 
+ * @param count 
+ */
+void wait_ticks(int count)
+{
+	volatile int ticks = count;
+	while(ticks>0)
+		ticks--;
+}
+
+
+void wait_micros(int us)
+{
+    int ticks = (int)((us - 8.43) / 2.477);
+    wait_ticks(ticks);
+}
+
+/**
+ * @brief 
+ * 
+ */
 void config_adc()
 {
     //Set the PCADC bit
@@ -32,24 +60,43 @@ void config_adc()
 
     //Enable ADC0 pin 0
     PINSEL1 |= (1 << 14);
-
-    //Set channel 0 to have an interrupt
-    //AD0INTEN |= 1;
 }
+
+
+void config_dac()
+{
+    //Configure the clock source
+    PCLKSEL0 |= (1 << 22);
+
+    //Configure the PINSEL registers to enable AOUT
+    PINSEL1 |= (1 << 21);
+}
+
 
 int main(void)
 {
     /* Declare Local Variables */
     int light_status;
+    int doorbell_status;
+    int amp;
+
+    //Initialize doorbell_status to -1 to indicate start
+    doorbell_status = -1;
 
     //Set up the ADC device
     config_adc();
 
+    //Set up the DAC device
+    config_dac();
+
     //Configure the LED
     FIO0DIR |= (1 << 22);
     PINSEL1 &= ~(3 << 24);
-    FIO0PIN |= (1 << 22);
 
+    //Configure the Button
+    FIO2DIR &= ~(1 << 8);
+
+    //Run the program
     while(1)
     {
         //Start ADC conversion
@@ -62,7 +109,7 @@ int main(void)
         light_status = ((AD0DR0 >> 4) & (0x7FF));
 
         //Turn LED on or off based on the ADC reading
-        if(light_status > LIGHT_THRESH)
+        if(light_status <= LIGHT_THRESH)
         {
             FIO0PIN |= (1 << 22);
         }
@@ -71,7 +118,60 @@ int main(void)
             FIO0PIN &= ~(1 << 22);
         }
         
+        //Read the button
+        //If the button is pressed, then start the first bell
+        if(((FIO2PIN >> 8) & 1) && doorbell_status <= 0)
+        {
+            doorbell_status = 1;
+        }
+        //If the button is un-pressed then start second bell
+        else if(!((FIO2PIN >> 8) & 1))
+        {
+            if(doorbell_status > 0)
+            {
+                doorbell_status = 0;
+            }
+        }
 
+        //If the doorbell is on the first ring, ring bell 1 at 659 Hz
+        if(doorbell_status == 1)
+        {
+            //Scale amplitude down over time
+            for(amp = 1023; amp >= 0; amp-=4)
+            {
+                //Run 5 times at the same amplitude before dropping down one increment
+                for(int i = 0; i < 2; ++i)
+                {
+                    DACR = (amp << 6);
+                    wait_ticks(301);
+                    DACR = 0;
+                    wait_ticks(301);
+                }
+            }
+
+            //End of ring 1
+            doorbell_status = 2;
+        }
+        //Otherwise, ring bell 2 at 523 Hz
+        else if(doorbell_status == 0)
+        {
+            //Scale amplitude down over time
+            for(amp = 1023; amp >= 0; amp-=4)
+            {
+                //Run 5 times at the same amplitude before dropping down one increment
+                for(int i = 0; i < 2; ++i)
+                {
+                    DACR = (amp << 6);
+                    wait_ticks(381);
+                    DACR = 0;
+                    wait_ticks(381);
+                }
+            }
+
+            //End of ring 2
+            doorbell_status = -1;
+        }
+        
     }
 
     return 0 ;
