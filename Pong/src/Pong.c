64e7b8fa94cb89ced16ed9c8e06ec1cdee777128
jkleiber <jkleiber8@gmail.com>
2019-04-16 14:24:51 -0500

    Works on initializing the Pong stuff

--- Pong/src/Pong.c
+++ Pong/src/Pong.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 
+#include "GPIO.h"
 #include "register_defs.h"
 #include "SPI.h"
 
-int main(void) {
+//Program constants
+#define BALL_SIZE       2
+#define BALL_SPEED      2
+#define PADDLE_SPEED    1
+#define PADDLE_LENGTH   10
 
-    printf("Hello World\n");
+//Pin Definitions
+#define LCD_RES_PORT    2
+#define LCD_RES_PIN     7
 
-    //TODO: Pin 47 (P2.5) = CS, Pin 48 (P2.6) = D/C not, Pin 49 (P2.7) = RST
+//Variables for pong paddle tracking
+int paddle1_x = 5;
+int paddle1_y = 20;
+int paddle2_x = 79;
+int paddle2_y = 20;
+
+//Variables for pong ball tracking
+int ball_x = 24;
+int ball_y = 42;
+int ball_x_spd = 1;
+int ball_y_spd = 1;
+
+
+//TODO: Collision detection
+
+
+int main(void) 
+{
+    //Reset the LCD immediately
+    gpio_write_single(2, 7, 0); //RESET pin (P2.7)(49)
+
+    //TODO: Pin 47 (P2.5) = CS, Pin 48 (P2.6) = D/C not
 
     //TODO: Set up the audio output (DMA) and the sound effects
 
@@ -22,7 +50,16 @@ int main(void) {
     //Run the program infinitely
     while(1) 
     {
-        
+        //TODO: Update the locations of the ball
+
+        //TODO: Detect collisions and goals
+
+        //TODO: Change ball physics if needed
+
+        //TODO: Make sound if needed
+
+        //TODO: Update display
+
     }
 
     return 0;
